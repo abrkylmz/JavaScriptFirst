2d1cea9963567484572bf0d4c8d86f2014f2c9d4
abrkylmz <ahmettbylmz@gmail.com>
2022-06-16 09:14:54 +0300

    My first trying in JavaScript

---
+++ scripts/products.js
@@ -0,0 +1,128 @@
+var products = [
+  {
+     "id":"b2b8b02d-ba77-412b-ab56-4a6259de4b44",
+     "name":"LOOKlight Genius Jelly Brown Çocuk Güneş Gözlüğü",
+     "description":"<meta charset=\"utf-8\"><p><span>9-15 Yaş çocuk için uygundur. </span></p><p>KIRILMAZ: İsviçrede üretilen Grilamid Natural hammaddesi dışarıdan gelebilecek her türlü darbelere karşı esneyip eski halini alır. Bu esneklik sayesinde hem size zarar vermez hem de kendisi zarar görmez.<br> ANTİALERJİK: Grilamid Natural <meta charset=\"utf-8\"><span data-mce-fragment=\"1\">ham maddesi kansere</span> ve alerjiye neden olan hiç bir BISFENOL A (BPA) maddesi içermemektedir. %100 sağlıklıdır.<br> ANTİBAKTERİYEL: Bağımsız laboratuvarlar tarafından alınan sonuçlaGrilamid Natural hammedesinin steril bir özelliğe sahip olduğu kanıtlanmıştır.<br> HAFİF: Ağır gözlükler kullanıcıda baş ağrısı ve burun estetiğinde bozulmalara neden olur. Grilamid Natural hafif ama dayanıklı gözlükler üretmemizi sağlamaktadır.<br> Malzeme : Grilamid Nature (Switzerland)<br> Cam : CR39, 100% UV Koruyuculu, Polarize<br> Cam Rengi : Yeşil (G15)<br> Cinsiyet : Unisex<br> Çerçeve Rengi : Jel Kahverengi<br> Çerçeve Ölçüsü : 44mm (Cam) 18mm (Burunluk) 110mm (Sap)</p>",
+     "price":339,
+     "salePrice":"",
+     "stockCode":"Genius-M / 02 / G-15",
+     "brand":"LOOKlight",
+     "categories":"Gözlük",
+     "images":[
+        "https://cdn.myikas.com/images/dcf9a70d-8477-4704-be82-48609ce519f5/debb9df7-2325-43a9-8dc0-97e04b7c974b/image_1950.webp",
+        "https://cdn.myikas.com/images/dcf9a70d-8477-4704-be82-48609ce519f5/9aecb630-34ea-4f0f-a4ce-c33ef7018a18/image_1950.webp"
+     ]
+  },
+  {
+     "id":"c059915a-8e89-4fe1-8a63-26acbb1310df",
+     "name":"LOOKlight Genius Daisy Çocuk Güneş Gözlüğü",
+     "description":"<meta charset=\"utf-8\"><p><span>9-15 Yaş çocuk için uygundur. </span></p><p>KIRILMAZ: İsviçrede üretilen Grilamid Natural hammaddesi dışarıdan gelebilecek her türlü darbelere karşı esneyip eski halini alır. Bu esneklik sayesinde hem size zarar vermez hem de kendisi zarar görmez.<br> ANTİALERJİK: Grilamid Natural <meta charset=\"utf-8\"><span data-mce-fragment=\"1\">ham maddesi kansere </span>ve alerjiye neden olan hiç bir BISFENOL A (BPA) maddesi içermemektedir. %100 sağlıklıdır.<br> ANTİBAKTERİYEL: Bağımsız laboratuvarlar tarafından alınan sonuçlaGrilamid Natural hammedesinin steril bir özelliğe sahip olduğu kanıtlanmıştır.<br> HAFİF: Ağır gözlükler kullanıcıda baş ağrısı ve burun estetiğinde bozulmalara neden olur. Grilamid Natural hafif ama dayanıklı gözlükler üretmemizi sağlamaktadır.<br> Malzeme : Grilamid Nature (Switzerland)<br> Cam : CR39, 100% UV Koruyuculu, Polarize<br> Cam Rengi : Yeşil (G15)<br> Cinsiyet : Unisex<br> Çerçeve Rengi : Papatya<br> Çerçeve Ölçüsü : 44mm (Cam) 18mm (Burunluk) 110mm (Sap)</p>",
+     "price":339,
+     "salePrice":"",
+     "stockCode":"Genius-M / 07 / G-15",
+     "brand":"LOOKlight",
+     "categories":"Gözlük",
+     "images":[
+        "https://cdn.myikas.com/images/dcf9a70d-8477-4704-be82-48609ce519f5/22b69f24-aa39-4280-a78e-b7b3c0954d52/image_1950.webp",
+        "https://cdn.myikas.com/images/dcf9a70d-8477-4704-be82-48609ce519f5/9faddf68-5b5d-43bf-8560-b2a036701d25/image_1950.webp"
+     ]
+  },
+  {
+     "id":"1a0010d5-d72e-4495-9029-ff4d83b7326e",
+     "name":"LOOKlight Genius Cherry Çocuk Güneş Gözlüğü",
+     "description":"<p>9-15 yaş çocuk için uygundur.</p><p>KIRILMAZ: İsviçrede üretilen Grilamid Natural hammaddesi dışarıdan gelebilecek her türlü darbelere karşı esneyip eski halini alır. Bu esneklik sayesinde hem size zarar vermez hem de kendisi zarar görmez.</p><p> ANTİALERJİK: Grilamid Natural ham maddesi kansere ve alerjiye neden olan hiç bir BISFENOL A (BPA) maddesi içermemektedir. %100 sağlıklıdır.</p><p> ANTİBAKTERİYEL: Bağımsız laboratuvarlar tarafından alınan sonuçlaGrilamid Natural hammedesinin steril bir özelliğe sahip olduğu kanıtlanmıştır.</p><p> HAFİF: Ağır gözlükler kullanıcıda baş ağrısı ve burun estetiğinde bozulmalara neden olur. Grilamid Natural hafif ama dayanıklı gözlükler üretmemizi sağlamaktadır.</p><p> Malzeme : Grilamid Nature (Switzerland)</p><p> Cam : CR39, 100% UV Koruyuculu, Polarize</p><p> Cam Rengi : Mavi Degrede (LPU9)</p><p> Cinsiyet : Unisex</p><p> Çerçeve Rengi : Vişne</p><p> Çerçeve Ölçüsü : 44mm (Cam) 18mm (Burunluk) 110mm (Sap)</p>",
+     "price":339,
+     "salePrice":299,
+     "stockCode":"Genius-M / 09 / LPU-9",
+     "brand":"LOOKlight",
+     "categories":"Gözlük",
+     "images":[
+        "https://cdn.myikas.com/images/dcf9a70d-8477-4704-be82-48609ce519f5/244fb28e-23bc-448c-ab17-059ef00503d4/image_1950.webp",
+        "https://cdn.myikas.com/images/dcf9a70d-8477-4704-be82-48609ce519f5/6ff474a8-1623-4fff-8379-5d7b923d3a58/image_1950.webp"
+     ]
+  },
+  {
+     "id":"1af2021c-ce2c-4a87-a9ac-7c9b64b0b990",
+     "name":"LOOKlight Genius Lavender Çocuk Güneş Gözlüğü",
+     "description":"<p>9-15 yaş çocuk için uygundur.</p><p>KIRILMAZ: İsviçrede üretilen Grilamid Natural hammaddesi dışarıdan gelebilecek her türlü darbelere karşı esneyip eski halini alır. Bu esneklik sayesinde hem size zarar vermez hem de kendisi zarar görmez.</p><p> ANTİALERJİK: Grilamid Natural ham maddesi kansere ve alerjiye neden olan hiç bir BISFENOL A (BPA) maddesi içermemektedir. %100 sağlıklıdır.</p><p> ANTİBAKTERİYEL: Bağımsız laboratuvarlar tarafından alınan sonuçlaGrilamid Natural hammedesinin steril bir özelliğe sahip olduğu kanıtlanmıştır.</p><p> HAFİF: Ağır gözlükler kullanıcıda baş ağrısı ve burun estetiğinde bozulmalara neden olur. Grilamid Natural hafif ama dayanıklı gözlükler üretmemizi sağlamaktadır.</p><p> Malzeme : Grilamid Nature (Switzerland)</p><p> Cam : CR39, 100% UV Koruyuculu, Polarize</p><p> Cam Rengi : Füme (S10)</p><p> Cinsiyet : Unisex</p><p> Çerçeve Rengi : Lavanta</p><p> Çerçeve Ölçüsü : 44mm (Cam) 18mm (Burunluk) 110mm (Sap)</p>",
+     "price":339,
+     "salePrice":299,
+     "stockCode":"Genius-M / 17 / S-10",
+     "brand":"LOOKlight",
+     "categories":"Gözlük",
+     "images":[
+        "https://cdn.myikas.com/images/dcf9a70d-8477-4704-be82-48609ce519f5/ea6cb0db-60b3-4e32-aacd-4fd74640e663/image_1950.webp",
+        "https://cdn.myikas.com/images/dcf9a70d-8477-4704-be82-48609ce519f5/fb140b5f-b6e3-4c9b-8961-491443cf8284/image_1950.webp"
+     ]
+  },
+  {
+     "id":"9aba9578-b617-4162-a534-80fa9448e438",
+     "name":"LOOKlight Genius Indigo Çocuk Güneş Gözlüğü",
+     "description":"<p>9-15 yaş çocuk için uygundur.</p><p>KIRILMAZ: İsviçrede üretilen Grilamid Natural hammaddesi dışarıdan gelebilecek her türlü darbelere karşı esneyip eski halini alır. Bu esneklik sayesinde hem size zarar vermez hem de kendisi zarar görmez.<br> ANTİALERJİK: Grilamid Natural <meta charset=\"utf-8\"><span data-mce-fragment=\"1\">ham maddesi kansere</span> ve alerjiye neden olan hiç bir BISFENOL A (BPA) maddesi içermemektedir. %100 sağlıklıdır.<br> ANTİBAKTERİYEL: Bağımsız laboratuvarlar tarafından alınan sonuçlaGrilamid Natural hammedesinin steril bir özelliğe sahip olduğu kanıtlanmıştır.<br> HAFİF: Ağır gözlükler kullanıcıda baş ağrısı ve burun estetiğinde bozulmalara neden olur. Grilamid Natural hafif ama dayanıklı gözlükler üretmemizi sağlamaktadır.<br> Malzeme : Grilamid Nature (Switzerland)<br> Cam : CR39, 100% UV Koruyuculu, Polarize<br> Cam Rengi : Füme (S10)<br> Cinsiyet : Unisex<br> Çerçeve Rengi : Çivit<br> Çerçeve Ölçüsü : 44mm (Cam) 18mm (Burunluk) 110mm (Sap)</p>",
+     "price":339,
+     "salePrice":299,
+     "stockCode":"Genius-M / 19 / S-10",
+     "brand":"LOOKlight",
+     "categories":"Gözlük",
+     "images":[
+        "https://cdn.myikas.com/images/dcf9a70d-8477-4704-be82-48609ce519f5/08462c69-8552-44ba-a5db-4e71db9c1679/image_1950.webp",
+        "https://cdn.myikas.com/images/dcf9a70d-8477-4704-be82-48609ce519f5/5ed94dc5-09b7-4830-8fd0-b693926d64b7/image_1950.webp"
+     ]
+  },
+  {
+     "id":"ca55db8c-f866-49cb-9275-9f003165d0c5",
+     "name":"LOOKlight Hexagonal-M Matte Black Güneş Gözlüğü",
+     "description":"<p>Malzeme : Metal<br>Cam : CR39, 100% UV Koruyuculu, Polarize<br>Cam Rengi : Füme Degrede (G1)<br>Cinsiyet : Unisex<br>Çerçeve Rengi : Mat Siyah<br>Çerçeve Ölçüsü : 50mm (Cam) 22mm (Burunluk) 140mm (Sap)</p>",
+     "price":439,
+     "salePrice":"",
+     "stockCode":"Hexa-M / 03M / G-1",
+     "brand":"LOOKlight",
+     "categories":"",
+     "images":[
+        "https://cdn.myikas.com/images/dcf9a70d-8477-4704-be82-48609ce519f5/8caa4aa5-a188-4b90-b448-80d38290dc7d/image_1950.webp",
+        "https://cdn.myikas.com/images/dcf9a70d-8477-4704-be82-48609ce519f5/5311fc6b-d3bd-448e-abd3-1410c242b5bd/image_1950.webp"
+     ]
+  },
+  {
+     "id":"e37b8567-ea89-4339-bb9b-656d3907ba88",
+     "name":"LOOKlight Hexagonal-M Matte Black Güneş Gözlüğü",
+     "description":"<p>Malzeme : Metal<br>Cam : CR39, 100% UV Koruyuculu, Polarize<br>Cam Rengi : Yeşil (G15)<br>Cinsiyet : Unisex<br>Çerçeve Rengi : Mat Siyah<br>Çerçeve Ölçüsü : 50mm (Cam) 22mm (Burunluk) 140mm (Sap)</p>",
+     "price":439,
+     "salePrice":"",
+     "stockCode":"Hexa-M / 03M / G-15",
+     "brand":"LOOKlight",
+     "categories":"Erkek Gözlük",
+     "images":[
+        "https://cdn.myikas.com/images/dcf9a70d-8477-4704-be82-48609ce519f5/1062a77f-d3d4-4de7-bc51-c7b6a00edac4/image_1950.webp",
+        "https://cdn.myikas.com/images/dcf9a70d-8477-4704-be82-48609ce519f5/0dd47f62-00d6-446c-924a-c4c14f60eeaa/image_1950.webp"
+     ]
+  },
+  {
+     "id":"f5a32c5b-16a3-42f2-98fa-42fa55a419c8",
+     "name":"LOOKlight Hexagonal-M Matte Black Güneş Gözlüğü",
+     "description":"<p>Malzeme : Metal<br>Cam : CR39, 100% UV Koruyuculu, Polarize<br>Cam Rengi : Mavi (LGB32)<br>Cinsiyet : Unisex<br>Çerçeve Rengi : Mat Siyah<br>Çerçeve Ölçüsü : 50mm (Cam) 22mm (Burunluk) 140mm (Sap)</p>",
+     "price":439,
+     "salePrice":"",
+     "stockCode":"Hexa-M / 03M / LGB-32",
+     "brand":"LOOKlight",
+     "categories":"Erkek Gözlük",
+     "images":[
+        "https://cdn.myikas.com/images/dcf9a70d-8477-4704-be82-48609ce519f5/7d019dd8-aaec-4937-9ecf-1975a5345815/image_1950.webp",
+        "https://cdn.myikas.com/images/dcf9a70d-8477-4704-be82-48609ce519f5/a24a54f3-6547-4a8d-abdf-189152f8a6a8/image_1950.webp"
+     ]
+  },
+  {
+     "id":"cc7a1f3b-2644-4f97-8c39-5d424e5bc2c4",
+     "name":"LOOKlight Hexagonal-M Gold Güneş Gözlüğü",
+     "description":"<p>• Malzeme : Metal</p><p>• Cam : CR39, 100% UV Koruyuculu, Polarize</p><p>• Cam Rengi : Kahverengi Degrede (DB9)</p><p>• Cinsiyet : Unisex</p><p>• Çerçeve Rengi : Gold</p><p>• Çerçeve Ölçüsü : 50mm (Cam) 22mm (Burunluk) 140mm (Sap)</p>",
+     "price":439,
+     "salePrice":"",
+     "stockCode":"Hexa-M / 13 / DB-09",
+     "brand":"LOOKlight",
+     "categories":"Erkek Gözlük",
+     "images":[
+        "https://cdn.myikas.com/images/dcf9a70d-8477-4704-be82-48609ce519f5/3ce42b35-3fc9-475d-9eef-1a4f756d037b/image_1950.webp",
+        "https://cdn.myikas.com/images/dcf9a70d-8477-4704-be82-48609ce519f5/04ff47fb-9938-4a8f-acfb-e3a2063c2e49/image_1950.webp"
+     ]
+  }
+];
